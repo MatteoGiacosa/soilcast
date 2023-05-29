564a6dffc9a9449f4e40920282e1f1f3f38eb0ba
Alex Puliatti <a.puliatti@hotmail.co.uk>
2023-05-29 17:07:11 +0200

    Notificationsssss

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -3,28 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\OneSignalService;
 
 class NotificationController extends Controller
 {
+    protected $oneSignalService;
+
+    public function __construct(OneSignalService $oneSignalService)
+    {
+        $this->oneSignalService = $oneSignalService;
+    }
+
+    public function getPreferences(Request $request)
+    {
+        // Assuming user_id is authenticated
+        $userId = $request->user()->id;
+
+        $user = User::findOrFail($userId);
+        return response()->json($user->only('irrigation_status', 'sensor_low_battery', 'no_signal'));
+    }
+
     public function updatePreferences(Request $request)
     {
-        // validate the request data
         $data = $request->validate([
-            'user_id' => 'required|exists:users,id', // assuming you are passing user's id
             'irrigation_status' => 'required|boolean',
             'sensor_low_battery' => 'required|boolean',
             'no_signal' => 'required|boolean',
         ]);
 
-        // find the user and update the preferences
-        $user = User::findOrFail($data['user_id']);
-        $user->update([
-            'irrigation_status' => $data['irrigation_status'],
-            'sensor_low_battery' => $data['sensor_low_battery'],
-            'no_signal' => $data['no_signal'],
+        $userId = $request->user()->id;
+        $user = User::findOrFail($userId);
+        $user->update($data);
+
+        return response()->json(['message' => 'Preferences updated successfully.']);
+    }
+
+    public function createNotification(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required|string',
+            'message' => 'required|string',
+            'users' => 'required|array', // assuming you are passing an array of user_ids to send notification to
         ]);
 
-        // return a response
-        return response()->json(['message' => 'Preferences updated successfully.']);
+        $notificationData = [
+            'headings' => [
+                'en' => $data['title'],
+            ],
+            'contents' => [
+                'en' => $data['message'],
+            ],
+            'include_player_ids' => $data['users'],
+        ];
+
+        $this->oneSignalService->sendNotification($notificationData);
+
+        return response()->json(['message' => 'Notification sent successfully.']);
     }
 }
