4181e0d9d8cbdea9b443bc2dc8dded43c3854bb2
Alex Puliatti <a.puliatti@hotmail.co.uk>
2023-05-29 10:38:42 +0200

    weatherLog7

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Weather;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class WeatherController extends Controller
 {
