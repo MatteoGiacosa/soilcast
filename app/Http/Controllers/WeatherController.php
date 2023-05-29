c9c7c15bcef4c358f8dfb2fe52c9b7f915adab56
Alex Puliatti <a.puliatti@hotmail.co.uk>
2023-05-29 10:30:04 +0200

    weatherLog5

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -14,20 +14,24 @@ class WeatherController extends Controller
             'zip' => $zipCode.','.$countryCode,
             'appid' => 'f4b6f7094c0e4bd9e24f56f5dff387ee',
         ]);
-
+    
         if (!$geolocationResponse->successful()) {
+            // Add this line for debugging
+            Log::error('Failed to fetch geolocation data', ['response' => $geolocationResponse->json()]);
             return response()->json(['error' => 'Unable to get geolocation data'], 500);
         }
-
+    
         $geolocationData = $geolocationResponse->json();
-
+    
         $weatherResponse = Http::get('http://api.openweathermap.org/data/2.5/weather', [
             'lat' => $geolocationData['lat'],
             'lon' => $geolocationData['lon'],
             'appid' => 'f4b6f7094c0e4bd9e24f56f5dff387ee',
         ]);
-
+    
         if (!$weatherResponse->successful()) {
+            // Add this line for debugging
+            Log::error('Failed to fetch weather data', ['response' => $weatherResponse->json()]);
             return response()->json(['error' => 'Unable to get weather data'], 500);
         }
 
