81fafd43a95cfe165c50da3b30c664b37d8c7dd0
Alex Puliatti <a.puliatti@hotmail.co.uk>
2023-05-29 14:38:36 +0200

    weatherLog9

--- app/Models/WeatherHistory.php
+++ app/Models/WeatherHistory.php
@@ -9,9 +9,15 @@ class WeatherHistory extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'weather_id',
+        'temperature',
+        'recorded_at',
+    ];
+
     public function weather()
     {
         return $this->belongsTo(Weather::class);
     }
-
 }
+
